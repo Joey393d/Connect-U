790c967b92395bf166d8cef93bc7735cf71eaab3
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-17 15:38:07 -0500

    create call for constructor function that creates new object for each organization

--- js/organizationdata.js
+++ js/organizationdata.js
@@ -1,14 +1,30 @@
 'use strict';
 console.log('organizationdata.js is now connected');
 
-function Organization(orgName, phoneNumber, address, category){
+function Organization(orgName, website, phoneNumber, address, category){
 this.orgName = orgName;
+this.website = website;
 this.phoneNumber = phoneNumber;
 this.address = address;
 this.category = category;
 }
 
-let housing = ['Cedar Valley Habitat for Humanity', 'Family Promise'];
+let cedarValley = new Organization('Cedar Valley Habitat for Humanity', 'https://www.cvhabitat.org/', '319-366-4485', '350 6th Ave SE Cedar Rapids, IA 52401', 'housing');
+
+let familyPromise = new Organization('Family Promise', 'https://familypromiseoflinncounty.org/', '319-540-6494', '610 31st Street SE Cedar Rapids, IA 52403', 'housing');
+
+let 
+
+let housing = [cedarValley, familyPromise];
 let food = ['Feed Iowa First', 'Linn County Food Bank'];
 let youth = ['Ronald McDonald House of Eastern Iowa', 'Dream Factory'];
 let animals = ['Last Hope Animal Rescue Inc', 'Cedar Rapids AniMeals'];
+
+console.log(housing);
+console.log(food);
+console.log(youth);
+console.log(animals);
+
+
+
+
