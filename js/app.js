f7f46c2f6d9208a099038c086795f1c92e0a2ab1
Joey <joseph.r.young2002@gmail.com>
2021-08-17 14:20:52 -0500

    finished form

--- js/app.js
+++ js/app.js
@@ -2,9 +2,24 @@
 console.log('app.js is now connected');
 
 
-const PH1 = document.getElementById('PH1');
 
-function Select() {
-  document.getElementById("Selection").submit()
-  console.log("answer is ", Select)
+
+function displayRadioValue(event) {
+  event.preventDefault();
+  console.log('this is the event',event);
+  let PH1result = event.target.Food.checked;
+  let PH2result = event.target.Youth.checked;
+  let PH3result = event.target.Animals.checked;
+  let HousingYN = event.target.Housing.checked;
+
+  console.log(PH1result, PH2result, PH3result, HousingYN);
+
+
+  // let form = getElementById('Selection');
+  // form.reset();
 }
+
+
+let formElement = document.getElementById('Selection');
+formElement.addEventListener('submit', displayRadioValue);
+
