c9153e4ac982b8b911cc555d8fc6f4d5b27ea0a7
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-17 15:42:27 -0500

    add objects for food category

--- js/organizationdata.js
+++ js/organizationdata.js
@@ -13,10 +13,12 @@ let cedarValley = new Organization('Cedar Valley Habitat for Humanity', 'https:/
 
 let familyPromise = new Organization('Family Promise', 'https://familypromiseoflinncounty.org/', '319-540-6494', '610 31st Street SE Cedar Rapids, IA 52403', 'housing');
 
-let 
+let feedIowa = new Organization('Feed Iowa First', 'https://www.feediowa1st.com/', '319-775-0149', 'PO Box 1190 Cedar Rapids, IA 52406-1190', 'food');
+
+let foodBank = new Organization('Linn County Food Bank', 'http://www.linncommunityfoodbank.org/Linn_Community_Food_Bank/Welcome.html', '319-364-3543', 'First Presbyterian Church 310 5th St. SE Cedar Rapids, IA, 52401', 'food');
 
 let housing = [cedarValley, familyPromise];
-let food = ['Feed Iowa First', 'Linn County Food Bank'];
+let food = [feedIowa, foodBank];
 let youth = ['Ronald McDonald House of Eastern Iowa', 'Dream Factory'];
 let animals = ['Last Hope Animal Rescue Inc', 'Cedar Rapids AniMeals'];
 
