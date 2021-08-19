014fed3580d30e48e3ca5856ff05202bf32ede9b
Misti Dinzy <mistidinzy@gmail.com>
2021-08-19 15:26:45 -0500

    refactoring js and index

--- js/name.js
+++ js/name.js
@@ -1,3 +1,6 @@
+'use strict';
+console.log('name.js file is connected');
+
 //name display
 let NameVar = localStorage.getItem('name');
-document.getElementById('namedisplay').innerHTML = 'Hello! ' + NameVar;
+document.getElementById('namedisplay').innerHTML = 'Hello ' + NameVar + '!';
